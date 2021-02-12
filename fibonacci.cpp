4b58715380598ebcf7bc281a1fc769ce7252ff6d
Mikey352 <mikeesquivel234@me.com>
2021-02-11 23:45:17 -0500

    added fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int fib[60];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i < 60; ++i){
+        fib[i] = fib[i-1] - fib[i-2];
+        std::cout << fib[i];
+    }
+
+}
